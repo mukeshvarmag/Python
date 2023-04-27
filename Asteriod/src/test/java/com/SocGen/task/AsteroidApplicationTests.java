120b327577c1bbc54826417dd86d94ddc0a5c8b3
Gugulothu Mukesh Varma <mukeshvarma.g@gmail.com>
2023-04-27 17:43:52 +0530

    Asteriod

---
+++ Asteriod/src/test/java/com/SocGen/task/AsteroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SocGen.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsteroidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Java/src/main/java/com/example/Java/loop/forloop.java
@@ -0,0 +1,11 @@
+package com.example.Java.loop;
+
+public class forloop {
+    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
